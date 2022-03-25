7067df0803d95b4da86b0d6a66a2e231a42d6b78
thisoban <thisoban_1995@hotmail.com>
2022-03-25 10:36:48 +0100

    tweaking code for setup database

---
+++ crypto/Interfaces/IcCyptoInterface.cs
@@ -0,0 +1,12 @@
+﻿using crypto.Entities;
+
+namespace crypto.Interfaces
+{
+    public interface IcCyptoInterface
+    {
+        Crypto GetCrypto(int id);
+        void AddCrypto(Crypto crypto);
+        void RemoveCrypto(int id);  
+        void UpdateCrypto(Crypto crypto);
+    }
+}
--- crypto/Program.cs
+++ crypto/Program.cs
@@ -8,8 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>();
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
